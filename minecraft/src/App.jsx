7a7a6c8d4a5c50e85bb6b6652700ae69740d906d
Kevin Li <kevin.li20021106@gmail.com>
2022-10-04 22:17:00 -0400

    minecraft-react starter

---
+++ minecraft/src/App.jsx
@@ -0,0 +1,14 @@
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return (
+    <>
+      <Canvas>
+        <Sky/>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
