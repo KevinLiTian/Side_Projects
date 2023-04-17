508577b2a033ea252912c348e043c61288e39722
KevinLiTian <kevin.li20021106@gmail.com>
2023-04-16 23:20:02 -0400

    Create person.py

---
+++ Talent_VS_Luck/person.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, _talent, _age=20, _wealth=10):
+        # Individual properties
+        self.talent = _talent
+        self.age = _age
+        self.wealth = _wealth
+
+        # Encountered events
+        self.events = []
